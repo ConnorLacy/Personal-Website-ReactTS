684ec64f80d251a6ae467e2ad911284fce2ef5b6
Connor Lacy <connorlacy@connors-mbp.lan>
2019-08-03 15:53:05 -0400

    Basic Formatting

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import ScrollSnap from 'scroll-snap';
+import './App.scss';
+import Projects from './Projects';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="AppContainer">
+      {/* <section className="Header">
+        <h1>Header items</h1>
+      </section> */}
+      {/* <div className="Body"> */}
+        <section className="Tile1">
+          <h1>Connor Lacy</h1>
+          <p>Some Foofoo quote</p>
+        </section>
+        <section className="Tile2">
+          <h1>Resume Stuff</h1>
+          <Projects/>
+        </section>
+        <section className="Tile3">
+          <h1>Projects and details</h1>
+        </section>
+        <section className="Tile4">
+          <h1>Social page</h1>
+        </section>
+      {/* </div> */}
+    </div> 
   );
 }
 
---
+++ personal-website/src/Projects.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Projects: React.FC = () => {
+    return (
+        <div>Projects Container</div>
+    );
+}
+
+export default Projects;
