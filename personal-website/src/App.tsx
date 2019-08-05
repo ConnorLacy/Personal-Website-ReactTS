f90b98a47ddccef9c503c4f9f25a0671355bc071
Connor Lacy <connorlacy@connors-mbp.lan>
2019-08-05 13:19:16 -0400

    Added css snap scrolling

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ScrollSnap from 'scroll-snap';
 import './App.scss';
 import Projects from './Projects';
 
 const App: React.FC = () => {
   return (
-    <div className="AppContainer">
-      {/* <section className="Header">
+    <div className="Page">
+      <header className="Header">
         <h1>Header items</h1>
-      </section> */}
-      {/* <div className="Body"> */}
-        <section className="Tile1">
-          <h1>Connor Lacy</h1>
-          <p>Some Foofoo quote</p>
-        </section>
-        <section className="Tile2">
-          <h1>Resume Stuff</h1>
-          <Projects/>
-        </section>
-        <section className="Tile3">
-          <h1>Projects and details</h1>
-        </section>
-        <section className="Tile4">
-          <h1>Social page</h1>
-        </section>
-      {/* </div> */}
+      </header>
+      <div className="page-body">
+        <div className="container y-scroll y-mandatory">
+            <div className="tile-wrapper">
+              <div className="tile">Title Page</div>
+              <div className="tile">About Me</div>
+              <div className="tile">Portfolio</div>
+              <div className="tile">Social Links</div>
+            </div>
+          </div>
+        </div>
     </div> 
   );
 }
