d2314804100b1e4757ca3c4ac943c50a7c5e13e9
Connor Lacy <connorlacy@connors-mbp.lan>
2019-08-05 15:35:01 -0400

    Added Functionality to nav bar buttons

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -2,24 +2,43 @@ import React from 'react';
 import './App.scss';
 import Projects from './Projects';
 
+const handleClick = (evt: any) => {
+  switch(evt.target.value){
+    case 1: window.location.hash = "#Title"
+            break;
+    case 2: window.location.hash = "#About"
+            break;
+    case 3: window.location.hash = "#Portfolio"
+            break;
+    case 4: window.location.hash = "#Social"
+            break;
+    
+  }
+}
+
 const App: React.FC = () => {
   return (
     <div className="Page">
       <header className="Header">
         <h1>Header items</h1>
       </header>
-      <div className="page-body">
-        <div className="container y-scroll y-mandatory">
-            <div className="tile-wrapper">
-              <div className="tile">Title Page</div>
-              <div className="tile">About Me</div>
-              <div className="tile">Portfolio
-              <Projects/></div>
-              <div className="tile">Social Links</div>
-            </div>
-          </div>
+      <div className="container y-scroll y-proximity">
+        <div className="sidenav">
+          <ul>
+            <li value={1} onClick={handleClick}></li>
+            <li value={2} onClick={handleClick}></li>
+            <li value={3} onClick={handleClick}></li>
+            <li value={4} onClick={handleClick}></li>
+          </ul>
         </div>
-    </div> 
+        <div className="tile-wrapper">
+          <div className="tile" id="Title">Title Page</div>
+          <div className="tile" id="About">About Me</div>
+          <div className="tile" id="Portfolio">Portfolio<Projects/></div>
+          <div className="tile" id="Social">Social Links</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
